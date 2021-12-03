c722604966cb5b37cda309352473295a99bbc319
Cristiano Campos de Souza <cristiano.souzac@hotmail.com.br>
2021-12-02 21:56:47 -0300

    Content Negociation

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,18 @@
+﻿//using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO
+    {
+        //[JsonPropertyName("code")] -> Mudar o nome
+        public int Id { get; set; }
+
+        //[JsonPropertyName("name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //[JsonIgnore] -> Não vai ser serializado
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
